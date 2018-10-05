984eab8e931031c3c415ac9bf149c4ada887de68
Matias Pan <matias.pan26@gmail.com>
2018-10-05 12:51:35 -0300

    add docker build

--- go.mod
+++
@@ -1,1 +0,0 @@
-module github.com/InternetOfThings/face-tracking-turret
--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+
+	"github.corp.globant.com/InternetOfThings/face-tracking-turret/server"
+)
+
+func main() {
+	// TODO: flags with specific platform to run
+
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	s := &server.Server{}
+	go func() {
+		<-c
+		s.Close()
+	}()
+
+	log.Fatal(s.Serve(":8080", "/", 0, "res10_300x300_ssd_iter_140000.caffemodel", "deploy.prototxt"))
+}
